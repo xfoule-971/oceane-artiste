b77819dba5e4a170dd86e0de96371a98d064aa12
xfoule <x.foule@gmail.com>
2026-02-09 15:22:50 -0400

    les fichiers du dossier services

---
+++ backend/services/Annee2024.service.js
@@ -0,0 +1,15 @@
+const pool = require('../db/db'); // fichier db.js qui exporte le pool mysql2/promise
+
+// Récupérer toutes les oeuvres de l'année 2024
+const getAllArtisans = async () => {
+    try {
+        const [rows] = await pool.query('SELECT * FROM annee_2024');
+        return rows;
+    } catch (err) {
+        throw new Error('Erreur lors de la récupération des oeuvres : ' + err.message);
+    }
+};
+
+module.exports = {
+    getAllArtisans,
+};
